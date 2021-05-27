4bd84fa74e196f015d10a8c92864e7acd482e464
StringWolfin <83752942+StringWolfin@users.noreply.github.com>
2021-05-27 10:38:25 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,24 +11,30 @@ public class Main {
 		
 		String data;
 		int count = 0;
-		int mezeracount = 0;
+		int count2 = 0;
 		
-		 File fl = new File("C:\\Users\\bohac\\Downloads\\lm.txt");
+		 File fl = new File(".//lm.txt");
 		 try {
 			Scanner pepiq = new Scanner(fl);
 			
 			String regex = "\\s";
+			String regex2 = "\\b";
 		     Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		     Pattern pattern2 = Pattern.compile(regex2, Pattern.MULTILINE);
 		     
 			
 			while(pepiq.hasNextLine()) {
 				data = pepiq.nextLine();
+				Matcher matcher2 = pattern2.matcher(data);
+				while (matcher2.find()) {
+					count2++;
+				}
 				for(char a: data.toCharArray()) {
 					Matcher matcher = pattern.matcher(data);
 					while (matcher.find()) {
 						count++;
 				}
-				data="";
+				data="";	
 			}
 		}
 			
@@ -35,7 +42,8 @@ public class Main {
 			
 			e.printStackTrace();
 		}
-		 System.out.println(count);
+		 System.out.println("Počet mezer: " + count);
+		 System.out.println("Počet slov: " + count2/2);
 	}
 
 }
